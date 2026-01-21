35101a1f8cb71c930d1510f4aa4a65861cb33ef0
Dean Howe <hideanhowe@gmail.com>
2026-01-21 15:25:52 +0000

    solution

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 // Given two positive integers representing the lengths for the two legs (the two short sides) of a right triangle, determine whether the hypotenuse is an integer.
 
 // The length of the hypotenuse is calculated by adding the squares of the two leg lengths together and then taking the square root of that total (a2 + b2 = c2).
+
+function isIntegerHypotenuse(a, b) {
+  console.log(typeof Math.sqrt(a * a + b * b).toString());
+  // store as string
+  const str = Math.sqrt(a * a + b * b).toString();
+  // turn number into string to find if it contains a "."
+
+  const regex = /\./;
+
+  return !regex.test(str);
+
+  // return a;
+}
+
+isIntegerHypotenuse(2, 3);
+isIntegerHypotenuse(10, 10);
